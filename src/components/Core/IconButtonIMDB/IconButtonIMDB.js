dd713c3cd8e4a9276470a2930ffdd3bdfca8567d
Yousef Omar <yousefo1995@gmail.com>
2023-05-18 19:04:28 +0300

    WishListButton changed to IconButtonIMDB

---
+++ src/components/Core/IconButtonIMDB/IconButtonIMDB.js
@@ -0,0 +1,33 @@
+import { StyledIconButton } from "./style";
+
+const IconButtonIMDB = ({
+  children,
+  iconSize,
+  borderR,
+  showBorder,
+  iconColor,
+  width,
+  height,
+  bg,
+  hoverBg,
+  iconHover,
+  ...res
+}) => {
+  return (
+    <StyledIconButton
+      iconSize={iconSize}
+      borderR={borderR}
+      iconColor={iconColor}
+      width={width}
+      height={height}
+      bg={bg}
+      hoverBg={hoverBg}
+      iconHover={iconHover}
+      {...res}
+    >
+      {children}
+    </StyledIconButton>
+  );
+};
+
+export default IconButtonIMDB;
--- src/components/Core/WishlistButton/style.js
+++
@@ -1,5 +0,0 @@
-export const IconStyle = {
-  fontSize: 62,
-  border: "none",
-};
-export const iconButtonStyles = { borderRadius: "12px", padding: "0" };
