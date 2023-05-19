82e40b820b6baea19fcb405d9559486aa518b439
Yousef Omar <yousefo1995@gmail.com>
2023-05-19 18:02:16 +0300

    Show full card prop added to Movie card

--- src/components/MovieCard/MovieCard/style.js
+++ src/components/MovieCard/MovieCard/style.js
@@ -1,15 +1,18 @@
 import styled from "@emotion/styled";
-import WishlistButton from "../../Core/WishlistButton/WishlistButton";
+import IconButtonIMDB from "../../Core/IconButtonIMDB/IconButtonIMDB";
 import Card from "@mui/material/Card";
 
-export const StyledCard = styled(Card)(({ theme }) => ({
-  borderRadius: "0px",
+export const StyledCard = styled(Card)(({ theme, showFullCard }) => ({
+  borderTopLeftRadius: "0",
+  borderTopRightRadius: "0",
+  borderBottomLeftRadius: showFullCard ? "6px" : "0",
+  borderBottomRightRadius: showFullCard ? "6px" : "0",
   backgroundColor: "#1A1A1A",
   maxWidth: "232px",
   marginRight: "0.875rem",
 }));
 
-export const StyledWishlistBtn = styled(WishlistButton)(({ theme }) => ({
+export const StyledIconButtonIMDB = styled(IconButtonIMDB)(({ theme }) => ({
   position: "absolute",
   top: 0,
   left: 0,
