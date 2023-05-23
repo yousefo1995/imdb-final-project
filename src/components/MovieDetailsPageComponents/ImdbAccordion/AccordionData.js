65bf7fadd842c9795b7d2a4b25e04cd5170ba185
Yousef Omar <yousefo1995@gmail.com>
2023-05-24 00:23:46 +0300

    Create Accordion for the Movie page

---
+++ src/components/MovieDetailsPageComponents/ImdbAccordion/AccordionData.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Divider, Stack, Typography } from "@mui/material";
+
+const AccordionData = ({ creator, stars }) => {
+  return (
+    <Typography>
+      <Divider sx={{ bgcolor: "#7B7B7B" }} />
+      <Stack paddingTop="16px" paddingBottom="16px" flexDirection="row">
+        <Typography color="#FFF">Creator</Typography>
+        <Typography color="info.main" marginLeft={2}>
+          {creator}
+        </Typography>
+      </Stack>
+      <Divider sx={{ bgcolor: "#7B7B7B" }} />
+      <Stack paddingTop="16px" paddingBottom="16px" flexDirection="row">
+        <Typography color="#FFF">Stars</Typography>
+        <Typography color="info.main" marginLeft={2}>
+          {stars}
+        </Typography>
+      </Stack>
+      <Divider sx={{ bgcolor: "#7B7B7B" }} />
+    </Typography>
+  );
+};
+
+export default AccordionData;
