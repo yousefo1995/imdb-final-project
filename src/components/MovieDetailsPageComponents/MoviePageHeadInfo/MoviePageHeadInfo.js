4b9e112b6a3f49004d9142232b3e3a6501dbe1fd
Yousef Omar <yousefo1995@gmail.com>
2023-05-24 16:49:21 +0300

    Set more components in Movie Page

--- src/components/MovieDetailsPageComponents/MoviePageHeadInfo/MoviePageHeadInfo.js
+++ src/components/MovieDetailsPageComponents/MoviePageHeadInfo/MoviePageHeadInfo.js
@@ -1,22 +1,48 @@
 import React from "react";
-import { Stack, Box, Typography } from "@mui/material";
+import { Stack, Box, Typography, IconButton } from "@mui/material";
 import MovieRating from "../MovieRating/MovieRating";
+import StyledCardButton from "../../MovieCard/StyledCardButton/StyledCardButton";
+import CategoryOutlinedIcon from "@mui/icons-material/CategoryOutlined";
+import ShareIcon from "@mui/icons-material/Share";
+import IconButtonIMDB from "../../Core/IconButtonIMDB/IconButtonIMDB";
+import Subtitle from "../../Core/Subtitle/Subtitle";
 const MoviePageHeadInfo = ({
   MovieName = "Movie Name",
   year = "2000",
   showType = "Tv Show",
+  duration = "00",
 }) => {
   return (
     <Stack>
-      {/* <Stack
-        width="100%"
-        flexDirection="row"
-        justifyContent="space-between"
-        bgcolor="yellow"
-        height="60px"
-      >
-        ffffffffffffffffffffffffffffff
-      </Stack> */}
+      <Stack width="100%" flexDirection="row" justifyContent="space-between">
+        <Subtitle fontSize="16px" showStartIcon={false}>
+          Duration <Typography marginLeft={1}>{duration}</Typography>
+        </Subtitle>
+        <Stack flexDirection="row">
+          <StyledCardButton
+            color="#FFF"
+            width="124px"
+            bg="#212121"
+            hoverbg="#2C2C2C"
+          >
+            <CategoryOutlinedIcon
+              sx={{ marginRight: "4px" }}
+              color="#FFF"
+              fontSize="medium"
+            />
+            All topics
+          </StyledCardButton>
+          <IconButtonIMDB
+            iconSize="24px"
+            iconColor="#FFF"
+            width="32px"
+            bg="#212121"
+            hoverBg="#2C2C2C"
+          >
+            <ShareIcon />
+          </IconButtonIMDB>
+        </Stack>
+      </Stack>
       <Stack
         width="100%"
         flexDirection="row"
--- src/pages/MoviePage.js
+++ src/pages/MoviePage.js
@@ -1,14 +1,16 @@
 import React from "react";
 import IconCard from "../components/MovieDetailsPageComponents/IconCard/IconCard";
-import { Container, Grid, Typography, Box } from "@mui/material";
+import { Container, Grid, Typography, Box, Stack } from "@mui/material";
 import GridMoviePoster from "../components/MovieDetailsPageComponents/GridMoviePoster/GridMoviePoster";
 import ImdbChip from "../components/Core/ImdbChip/ImdbChip";
 import MovieRating from "../components/MovieDetailsPageComponents/MovieRating/MovieRating";
 import ImdbAccordion from "../components/MovieDetailsPageComponents/ImdbAccordion/ImdbAccordion";
 import AccordionData from "../components/MovieDetailsPageComponents/ImdbAccordion/AccordionData";
 import MoviePageHeadInfo from "../components/MovieDetailsPageComponents/MoviePageHeadInfo/MoviePageHeadInfo";
+import StyledCardButton from "../components/MovieCard/StyledCardButton/StyledCardButton";
 import "./style/style.css";
-
+import AddIcon from "@mui/icons-material/Add";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 const MoviePage = ({ creator = "creator name", stars = "stars names" }) => {
   return (
     <Box className="gradiant">
@@ -17,10 +19,15 @@ const MoviePage = ({ creator = "creator name", stars = "stars names" }) => {
         <MoviePageHeadInfo />
         <Grid container spacing={0.5}>
           <Grid item display={{ xs: "none", md: "block" }} lg={3} md={3}>
-            <GridMoviePoster />
+            <GridMoviePoster showPlayTrailerBtn={false} />
           </Grid>
           <Grid item xs={12} md={9} lg={7}>
-            <Box width="100%" height="300px" bgcolor="blue"></Box>
+            <Box bgcolor="blue">
+              <GridMoviePoster
+                image="https://picsum.photos/200"
+                showWishlistBtn={false}
+              />
+            </Box>
           </Grid>
           <Grid item container md={12} lg={2} spacing={0.5}>
             <Grid item xs={6} lg={12}>
@@ -38,9 +45,9 @@ const MoviePage = ({ creator = "creator name", stars = "stars names" }) => {
               display={{ xs: "block", md: "none" }}
               xs={3}
             >
-              <GridMoviePoster />
+              <GridMoviePoster showPlayTrailerBtn={false} />
             </Grid>
-            <Grid item xs={8.25} md={8} lg={8}>
+            <Grid item xs={8.5} md={12} lg={8}>
               <Box flexWrap="wrap">
                 {/* category component */}
                 <ImdbChip>Drama</ImdbChip>
@@ -68,12 +75,54 @@ const MoviePage = ({ creator = "creator name", stars = "stars names" }) => {
             >
               <MovieRating />
             </Grid>
+            <Grid item container justifyContent={{ lg: "space-between" }}>
+              <Grid item xs={8.5} md={12} lg={8}>
+                <ImdbAccordion stars={stars} creator={creator} />
+              </Grid>
+              <Grid item md={8} display={{ xs: "none", md: "block" }}>
+                <Box marginTop={2} display={{ xs: "none", lg: "block" }}>
+                  <AccordionData stars={stars} creator={creator} />
+                </Box>
+              </Grid>
+              <Grid item xs={12} lg={3.75}>
+                <Stack flexDirection="row" marginTop={2}>
+                  <Box marginRight="2px">
+                    <StyledCardButton
+                      rightBorderRadious={false}
+                      bg="#3A3A3A"
+                      width="244px"
+                      color="#fff"
+                    >
+                      <AddIcon fontSize="small" />
+                      Add to WatchList
+                    </StyledCardButton>
+                  </Box>
+
+                  <StyledCardButton
+                    leftBorderRadious={false}
+                    bg="#3A3A3A"
+                    width="48px"
+                    color="#fff"
+                  >
+                    <KeyboardArrowDownIcon fontSize="small" />
+                  </StyledCardButton>
+                </Stack>
+                <Stack marginTop={2} flexDirection="row" alignItems="center">
+                  <Typography
+                    fontSize="16px"
+                    fontWeight="700"
+                    color="info.main"
+                  >
+                    0.0k
+                  </Typography>
+                  <Typography marginLeft={1} fontSize="14px" color="info.main">
+                    User reviews
+                  </Typography>
+                </Stack>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
-        <ImdbAccordion stars={stars} creator={creator} />
-        <Box marginTop={2} display={{ xs: "none", lg: "block" }}>
-          <AccordionData stars={stars} creator={creator} />
-        </Box>
       </Container>
     </Box>
   );
