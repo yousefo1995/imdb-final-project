63c33f557fa13a4d49a7319d775f7cfc20385e5b
Yousef Omar <yousefo1995@gmail.com>
2023-05-25 20:11:16 +0300

    Add width prop to ImdbButton

--- src/components/Core/ImdbButton/style.js
+++ src/components/Core/ImdbButton/style.js
@@ -2,13 +2,13 @@ import { Button } from "@mui/material";
 import { styled } from "@mui/system";
 
 export const StyledButton = styled(Button)(
-  ({ theme, padding, showborder, color, bg, bghover }) => ({
+  ({ theme, width, showborder, color, bg, bghover }) => ({
     cursor: "pointer",
     paddingTop: "4px",
     paddingBottom: "4px",
     border: showborder ? "1px solid black" : "none",
-    paddingLeft: padding,
-    paddingRight: padding,
+    width: width,
+    boxSizing: "border-box",
     color: color || "#000",
     backgroundColor: bg || "#f5c518",
     "&:hover": {
