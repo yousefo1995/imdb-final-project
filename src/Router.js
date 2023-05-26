3abf11c3563a676f1d9a80e3071eb0424773bb3b
Yousef Omar <yousefo1995@gmail.com>
2023-05-27 00:47:08 +0300

    Add private routes to router

--- src/Router.js
+++ src/Router.js
@@ -5,16 +5,26 @@ import LoginPage from "./pages/LoginPage";
 import MoviePage from "./pages/MoviePage";
 import SearchPage from "./pages/SearchPage";
 import WatchlistPage from "./pages/WatchlistPage";
+import SignUpPage from "./pages/SignUpPage";
+import NavBar from "./components/NavBar/NavBar";
+import PrivateRoutes from "./PrivateRoutes";
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="login" element={<LoginPage />} />
+        <Route element={<PrivateRoutes logged={false} to="/" />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+        </Route>
+
         <Route path="movie" element={<MoviePage />} />
         <Route path="search" element={<SearchPage />} />
-        <Route path="wlist" element={<WatchlistPage />} />
+        <Route element={<PrivateRoutes logged={true} to="/login" />}>
+          <Route path="wlist" element={<WatchlistPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
