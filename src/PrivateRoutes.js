ebf4e3b564629884a677218e1998d21fc3506d8a
Yousef Omar <yousefo1995@gmail.com>
2023-05-27 17:15:26 +0300

    Update private routes

--- src/PrivateRoutes.js
+++ src/PrivateRoutes.js
@@ -3,12 +3,12 @@ import { Outlet } from "react-router";
 import { useContext } from "react";
 import { AuthContext } from "./AuthContext";
 import { Navigate } from "react-router";
-const PrivateRoutes = ({ to, logged }) => {
+const PrivateRoutes = ({ to, isLogged }) => {
   const { currentUser } = useContext(AuthContext);
-  if (logged) {
-    return currentUser ? <Outlet /> : <Navigate to={to} />;
-  } else if (!logged) {
+  if (isLogged) {
     return !currentUser ? <Outlet /> : <Navigate to={to} />;
+  } else if (!isLogged) {
+    return currentUser ? <Outlet /> : <Navigate to={to} />;
   }
 };
 
