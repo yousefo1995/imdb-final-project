768fc536b149ae08cbcf0fe8082ec0c5a08b7e7d
Yousef Omar <yousefo1995@gmail.com>
2023-05-31 23:13:22 +0300

    Add Not Found Page to Router

--- src/Router.js
+++ src/Router.js
@@ -4,10 +4,11 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import MoviePage from "./pages/MoviePage";
 import SearchPage from "./pages/SearchPage";
-import WatchlistPage from "./pages/WatchlistPage";
+import WatchListPage from "./pages/WatchListPage";
 import SignUpPage from "./pages/SignUpPage";
 import NavBar from "./components/NavBar/NavBar";
 import PrivateRoutes from "./PrivateRoutes";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const Router = () => {
   return (
@@ -21,10 +22,12 @@ const Router = () => {
         </Route>
 
         <Route path="movie" element={<MoviePage />} />
-        <Route path="search" element={<SearchPage />} />
+        <Route path="search/:searchText" element={<SearchPage />} />
+        {/* <Route path="search/*" element={<h1>no search text</h1>} /> */}
         <Route element={<PrivateRoutes isLogged={false} to="/login" />}>
-          <Route path="wlist" element={<WatchlistPage />} />
+          <Route path="wlist" element={<WatchListPage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
