442946bab2e769b0940a758f4b4f3e8487539416
Yousef Omar <yousefo1995@gmail.com>
2023-06-02 16:35:31 +0300

    Fetch data for header section

--- src/components/HomePageComponents/HeroSliderSec/HeroSliderSec.js
+++ src/components/HomePageComponents/HeroSliderSec/HeroSliderSec.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
 import SimpleSlider from "../../SimpleSlider/SimpleSlider";
 import HeaderCard from "../../HeaderCard/HeaderCard";
-import { arr } from "../Draft";
+import axios from "axios";
 
 const HeroSliderSec = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/movie/upcoming",
+      params: { language: "en-US", page: "1" },
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzdiN2RmZTA2NGQ2MzZhOWRlNWIxYmUzYWVjZjc0OCIsInN1YiI6IjY0NjBiNWY4YTY3MjU0MDBlM2QxYzhkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FEW-f0nD7r9Pt2Y0z5zNp6haKVhqashRIv0aL6aU6LM",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        setList(response.data.results);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
   return (
     <Stack maxWidth={{ lg: "65%" }} width="100%">
       <Stack maxWidth={{ md: "100%", lg: "678px", xl: "848px" }}>
@@ -24,9 +46,22 @@ const HeroSliderSec = () => {
           ButtonsDisplay={{ xs: "none", md: "block" }}
           buttonHeight={{ sm: "45%", md: "50%", lg: "55%", xl: "65%" }}
         >
-          {arr.map((item) => (
-            <HeaderCard />
-          ))}
+          {list.map((item) => {
+            const posterPath = item.poster_path;
+            const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
+            const backdropPath = item.poster_path;
+            const backdropUrl = `https://image.tmdb.org/t/p/w500${backdropPath}`;
+            return (
+              <Stack key={item.id}>
+                <HeaderCard
+                  title={item.title}
+                  backdropImage={backdropUrl}
+                  posterImage={posterUrl}
+                  key={item.id}
+                />
+              </Stack>
+            );
+          })}
         </SimpleSlider>
       </Stack>
     </Stack>
--- src/components/HomePageComponents/TrailersSec/TrailersSec.js
+++ src/components/HomePageComponents/TrailersSec/TrailersSec.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Stack } from "@mui/material";
 import Subtitle from "../../Core/Subtitle/Subtitle";
 import Title from "../../Core/Title/Title";
 import TrailerCard from "../../TrailerCard/TrailerCard";
-
+import axios from "axios";
 const TrailersSec = () => {
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url: "https://api.themoviedb.org/3/movie/upcoming",
+      params: { language: "en-US", page: "2" },
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYzdiN2RmZTA2NGQ2MzZhOWRlNWIxYmUzYWVjZjc0OCIsInN1YiI6IjY0NjBiNWY4YTY3MjU0MDBlM2QxYzhkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FEW-f0nD7r9Pt2Y0z5zNp6haKVhqashRIv0aL6aU6LM",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        const NewList = response.data.results.slice(5, 8);
+        setList(NewList);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }, []);
   return (
     <Stack marginTop={1} paddingLeft={2} display={{ xs: "none", lg: "block" }}>
       <Title marginBottom={3} fontSize="20px">
         Up next
       </Title>
-      <TrailerCard />
-      <TrailerCard />
-      <TrailerCard />
+      {list.map((item) => {
+        const imagePath = item.poster_path;
+        const imageUrl = `https://image.tmdb.org/t/p/w500${imagePath}`;
+        return (
+          <Stack key={item.id}>
+            <TrailerCard
+              title={item.title}
+              image={imageUrl}
+              description={item.overview}
+              key={item.id}
+            />
+          </Stack>
+        );
+      })}
       <Subtitle showStartIcon={false} marginTop={2} minWidth="100%">
         Browse trailers
       </Subtitle>
