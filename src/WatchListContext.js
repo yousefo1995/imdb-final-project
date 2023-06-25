c20f8a92e2e385ac984d1d79e64cf036052e95e1
Yousef Omar <yousefo1995@gmail.com>
2023-06-26 01:38:38 +0300

    Add collection for every user using user email

--- src/WatchListContext.js
+++ src/WatchListContext.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { createContext } from "react";
 import {
   collection,
@@ -8,13 +8,22 @@ import {
   deleteDoc,
 } from "firebase/firestore";
 import { db } from "./Firebase";
+import { AuthContext } from "./AuthContext";
 
 export const WatchListContext = createContext();
 //
 const WatchListContextProvider = ({ children }) => {
   const [watchList, setWatchList] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+  const [email, setEmail] = useState("watchlist");
+
+  useEffect(() => {
+    if (currentUser) {
+      setEmail(`${currentUser.email} watchlist`);
+    }
+  }, [currentUser]);
   const getData = async () => {
-    const data = await getDocs(collection(db, "watchList"));
+    const data = await getDocs(collection(db, email));
     const filteredData = data.docs.map((doc) => ({
       ...doc.data(),
       firebaseId: doc.id,
@@ -23,13 +32,13 @@ const WatchListContextProvider = ({ children }) => {
   };
   useEffect(() => {
     getData();
-  }, []);
+  }, [email]);
 
   const addToWatchList = async (movieData) => {
     const isDuplicate = watchList.find((movie) => movie.id === movieData.id);
     if (!isDuplicate) {
       try {
-        const docRef = await addDoc(collection(db, "watchList"), movieData);
+        const docRef = await addDoc(collection(db, email), movieData);
         getData();
       } catch (e) {
         console.error("Error adding document: ", e);
@@ -41,14 +50,14 @@ const WatchListContextProvider = ({ children }) => {
     // setWatchList((prev) =>
     //   prev.filter((movie) => movie.firebaseId !== firebaseId)
     // );
-    const movieDoc = doc(db, "watchList", firebaseId);
+    const movieDoc = doc(db, email, firebaseId);
     await deleteDoc(movieDoc);
     getData();
   };
 
   const clearWatchList = async () => {
     const deletePromises = watchList.map(async (movie) => {
-      const movieDoc = doc(db, "watchList", movie.firebaseId);
+      const movieDoc = doc(db, email, movie.firebaseId);
       await deleteDoc(movieDoc);
     });
     await Promise.all(deletePromises);
